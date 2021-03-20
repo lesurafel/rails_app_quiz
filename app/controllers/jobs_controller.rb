10ed60755c63ac749b0e8f51af417dd9929689de
lesurafel <lesurafel@gmail.co>
2021-03-19 20:34:54 -0700

    Create router, controlle content and view files

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,10 +1,23 @@
 class JobsController < ApplicationController
   def index
+    @jobs = Job.all
+    render 'jobs/index'
   end
 
   def show
+    @job = Job.find_by(id: params[:id])
+    render 'jobs/create_and_show'
   end
 
   def create
+    @job = Job.new(job_params)
+    if @job.save
+      render 'jobs/create_and_show'
+    end
   end
+
+  private 
+    def job_params
+      params.require(:job).permit(:url, :employer_name, :employer_description, :job_title, :job_description, :year_of_experience, :education_requirement, :industry, :base_salary, :employment_type_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'jobs#index'
   get 'jobs' => 'jobs#index'
   get '/jobs/:id' => 'jobs#show'
   post 'jobs' => 'jobs#create'
